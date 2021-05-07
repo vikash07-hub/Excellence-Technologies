26cb11aa85e27b8ded0a37c934c7bfd355daea70
vikash07-hub <vikashmahalingam@gmail.com>
2021-05-07 17:12:38 +0530

    todo project

--- Final/todolist/src/App.js
+++ Final/todolist/src/App.js
@@ -1,23 +1,35 @@
+
+import React, {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
-
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 function App() {
+
+const[newtask,setnewtask]=useState('')
+const[tasks,settasks]=useState([])
+
+function addtask(){
+  settasks([...tasks, newtask])
+  }
+
+  
+
+const tasklist = tasks.map((object, index)=>{
+   return <div className="row justify-content-center"> 
+     <h1 className="col-md-6 text-left">{index+1} {object}</h1>
+    
+   </div>
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Todo List</h1>
+    <div className="row justify-content-center">
+      <input type="text" placeholder="Task to been assign" className="form-control col-md-6 m-1"  value={newtask} onChange={(e)=>{setnewtask(e.target.value)}}/>
+       <button onClick={addtask} className="btn btn-success col-md-1 m-1">ADD TASK</button>
+    </div>
+    
+    {tasklist}
     </div>
   );
 }
